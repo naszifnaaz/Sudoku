cb6a71e308e560a7af85ac966fe16717f49a6e8f
Muhammed Nasif <73707865+naszifnaaz@users.noreply.github.com>
2020-10-31 14:04:11 +0530

    added valid()
    added valid() to check for a valid solution

--- sudoku(text).py
+++ sudoku(text).py
@@ -40,3 +40,30 @@ def find_empty(bo):
         for j in range(len(bo[0])):
             if bo[i][j]==0:
                 return(i,j)
+    return None
+
+#Checking for Valid solution
+
+def valid(bo,num,pos):
+
+    #Check Rows
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i :
+            return False
+
+    #Check Column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] !=i :
+            return False
+                
+    #Checking 3x3 grid
+    box_x = pos[0] // 3
+    box_y = pos[1] // 3
+
+    for i in range(box_x * 3, box_x * 3 + 3):
+        for j in range(box_y * 3, box_y * 3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+    
+    return True
+
