546ad12b58838cd90fbe4b7a6fd800cc32e8f844
naszifnaaz <naszifnaaz@gmail.com>
2020-11-01 23:55:58 +0530

    added collision

--- sudoku(gui).py
+++ sudoku(gui).py
@@ -47,8 +47,10 @@ class Grid:
         row, col = self.selected
         self.cubes[row][col].set_temp(val)
 
-    #Drawing grid 
+    #Drawing template
     def draw(self, win):
+
+        #Draw grid lines
         gap = self.width / 9
         for i in range(self.row + 1):
             if i % 3 == 0 and i != 0:
@@ -57,3 +59,39 @@ class Grid:
                 thick = 1
             pygame.draw.line(win, (0,0,0), (0, i *gap), (self.width,i * gap), thick)
             pygame.draw.line(win, (0,0,0), (i * gap, 0), (i * gap, self.height), thick)
+
+        #Draw Cubes
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].draw(win)
+
+    #Reset    
+    def select(self, row, col):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].selected = False
+        self.cubes[row][col].selected = True
+        self.selected = (row, col)
+
+    def clear(self):
+        row, col = self.selected
+        if self.cubes[row][col].value == 0:
+            self.cubes[row][col].set_temp(0)
+    
+    #Collision detection
+    def click(self, pos):
+        if pos[0] < self.width and pos[1] < self.height:
+            gap = self.width / 9
+            x = pos[0] // gap
+            y = pos[1] // gap
+            return (int(y), int(x))
+        else:
+            return None
+    
+    def is_finished(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.cubes[i][j].value == 0:
+                    return False
+        return True
+        
